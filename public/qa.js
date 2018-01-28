c9c2fda291df24a18c6786890001c0f8df9fb592
Maria Dunning <mdunn09045@gmail.com>
2018-01-28 08:00:41 -0600

    Add relevant things

--- public/qa.js
+++ public/qa.js
@@ -1,7 +1,23 @@
 async function main() {
-  const color = await answer('What is your favorite color', ['purple', 'yellow'])
-  if (color === 'purple') {
-    const typeOfPurple = await answer('What kind of purple', ['dark purple', 'light purple'])
+  const sport = await answer('What sport are you playing?', ['Football', 'Volleyball'])
+  if (sport === 'Football') {
+    const footballOption = await answer('What kind of injuries do you want to stop?', ['Teeth Being Knocked Out', 'Bruising from Collisions', 'Jewels Getting Smashed'])
+    if(footballOption==='Teeth Being Knocked Out')
+    {
+      const mouthguardType= await answer( 'You should get a mouthgaurd. Pick an option',['Cheap','Better Fit','Expensive'])
+      if(mouthguardType==='Cheap')
+        suggest('Get a stock mouthguard','url for image', 'url to go to when click image')
+      else if(mouthguardType==='Better Fit')
+        suggest('Get a boil and bite mouthguard','url','urlforclick')
+      else
+        suggest('Get a custom fit mouthguard','url','urlforclick')
+    }
+    else if(footballOption==='Bruising from Collisions')
+    {
+      const position=await answer('What position do you play?', ['Quarterback','Running Back','Fullback/Linebacker','Lineman']) 
+      if(position==='Quarterback') {}
+    }
+      
     // ... continue
   }
   else if (answer === 'yellow') {
